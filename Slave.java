de19b7ff9b4dfdf7b9a277563dcc7e0313616ebd
vectorxz <vectorx2263@hotmail.com>
2021-01-21 23:11:15 +0700

    implement

---
+++ Slave.java
@@ -0,0 +1,8 @@
+class Slave {
+    public static void main(String args[]) {
+        /*
+            Change word after issue #10 is fixed
+        */
+        System.out.println("Slave says Hi!");
+    }
+}

c798a40a7806722730edc66d4d3aa524815c7cb0
Patiphol Pussawong <vectorx2263@hotmail.com>
2021-04-08 16:28:24 +0700

    Update Say.java

--- Say.java
+++ Say.java
@@ -3,4 +3,9 @@ class Say {
     // Change way of saying "BYE" after issue #64 is fixed
 		System.out.println("B"+"Y"+"E");
 	}
+	
+	// Change way to say "NICE!" after issue #65 is resolved
+	public static void sayNice() {
+		System.out.println("Ni"+"ce"+"!");
+	}
 }
